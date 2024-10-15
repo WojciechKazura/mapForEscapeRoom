98d9c424786620c163390b1effc9572cc50b6efb
WojciechKazura <wojciech.kazura@wp.pl>
2024-10-15 11:26:45 +0200

    Include rooms to game.

--- src/main/java/com/escapeRoomMap/GameService.java
+++ src/main/java/com/escapeRoomMap/GameService.java
@@ -10,50 +10,49 @@ public class GameService {
     private RoomRepository roomRepository;
     private GameRepository gameRepository;
 
-
     public GameService(RoomRepository roomRepository, GameRepository gameRepository) {
         this.roomRepository = roomRepository;
         this.gameRepository = gameRepository;
-
-
     }
 
     GameDTO saveGame(GameDTO gameDTO) {
         Game game = new Game(gameDTO.getName(), gameDTO.getHowManyRooms());
         gameRepository.save(game);
-        gameDTO.setFirstRoom(game.getFirtRoom().covertRoomToRoomDTO());
-        gameDTO.setId(game.getId());
-        gameDTO.setConnections(getConnections(game.getId()));
-        gameDTO.setActiveRoom(game.getActiveRoom().getId());
-        return gameDTO;
+        return mapToDTO(game);
     }
 
     GameDTO getGame(int id) {
         Game game = gameRepository.getReferenceById(id);
-        return new GameDTO(id, game.getName(),
+        return mapToDTO(game);
+    }
+
+    GameDTO mapToDTO(Game game) {
+        return new GameDTO(game.getId(), game.getName(),
                 game.getHowManyRooms(),
                 game.getFirtRoom().covertRoomToRoomDTO(),
                 getConnections(game.getId()),
-                game.getActiveRoom().getId());
+                game.getActiveRoom().getId(),
+                game.getFirtRoom().getConnectedRoomsId());
     }
 
 
     List<ConnectionView> getConnections(int gameId) {
         return roomRepository.getConnectionsView(gameId);
     }
+
     void move(int nextRoomId, int gameId) {
         Game game = gameRepository.findById(gameId).orElseThrow();
         List<ConnectionView> connections = getConnections(gameId);
         boolean isConnection = false;
         for (ConnectionView connection : connections) {
-            if (connection.getConnections().containsAll(List.of(nextRoomId,game.getActiveRoom().getId()))){
+            if (connection.getConnections().containsAll(List.of(nextRoomId, game.getActiveRoom().getId()))) {
                 isConnection = true;
             }
         }
-        if(isConnection){
+        if (isConnection) {
             game.setActiveRoom(roomRepository.findById(nextRoomId).orElseThrow());
             gameRepository.save(game);
-        }else{
+        } else {
             throw new IllegalStateException("Nie udało się wykonać takiego ruchu");
         }
 
